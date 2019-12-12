60ed2ddeec960b841d11d6e56657934e9969ae66
Ryan Wolande <hi@ryanwolande.com>
2019-12-11 21:05:44 -0800

    typo fix

--- api/controllers/register.py
+++ api/controllers/register.py
@@ -6,7 +6,7 @@ from flask_mysqldb import MySQL
 from flask import g
 
 from api import constants
-from api.controllers.base import BaseControlle
+from api.controllers.base import BaseController
 from api.status_codes import Status
 from api.controllers import db_query_select, db_query_insert
 
